b335509cde999561bee5d658751854efe1eec5fb
Melquisedeque Pereira <melqui1@live.com.pt>
2020-02-25 14:38:14 +0000

    Added max heap initial code

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,6 @@
-class Heap:
-    def __init__(self):
-        self.storage = []
-
-    def insert(self, value):
-        pass
-
-    def delete(self):
-        pass
-
-    def get_max(self):
-        pass
-
-    def get_size(self):
-        pass
-
-    def _bubble_up(self, index):
-        pass
-
-    def _sift_down(self, index):
-        pass
+from generic_heap import Heap as GenericHeap
+class Heap(GenericHeap):
+  def get_max(self):
+    pass
+  def comparator(self, v1, v2):
+    return v1 > v2
