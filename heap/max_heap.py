a2e6018c40f95d95127d7c712be7032891bdb105
Melquisedeque Pereira <melqui1@live.com.pt>
2020-02-25 14:57:11 +0000

    Modified max heap to inherit from generic heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,6 +1,7 @@
 from generic_heap import Heap as GenericHeap
 class Heap(GenericHeap):
+  def __init__(self):
+    super().__init__(lambda v1, v2: v1 > v2)
+
   def get_max(self):
     pass
-  def comparator(self, v1, v2):
-    return v1 > v2
