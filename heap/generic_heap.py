8897318a4c375671713afc79f7c3ed0d4b4fb181
Melquisedeque Pereira <melqui1@live.com.pt>
2020-02-26 14:53:46 +0000

    Completed `generic_heap` exercise

--- heap/generic_heap.py
+++ heap/generic_heap.py
@@ -20,13 +20,25 @@ class Heap:
   def _parentOf(self, index):
     return self.storage[self._parent(index)]
   def _left(self, index):
-    return (2 * index) + 1
+    value = (2 * index) + 1
+    if value > self.size:
+      return None
+    return value
   def _right(self, index):
-    return (2 * index) + 2
+    value = (2 * index) + 2
+    if value > self.size:
+      return None
+    return value
   def _leftOf(self, index):
-    return self.storage[self._left(index)]
+    index = self._left(index)
+    if index is not None:
+      return self.storage[index]
+    return None
   def _rightOf(self, index):
-    return self.storage[self._right(index)]
+    index = self._right(index)
+    if index is not None:
+      return self.storage[index]
+    return None
   def _isLeaf(self, index): 
     if index >= (self.size // 2) and index <= self.size: 
       return True
@@ -36,17 +48,21 @@ class Heap:
     self.size += 1
     self.storage.insert(self.size, value)
     self._bubble_up(self.size)
-    #print(f'insert {value} at {self.size}, {self.storage}')
 
+  """
+  Removes and returns the 'topmost' value from the heap; this method needs to ensure that the heap property is maintained after the topmost element has been removed. 
+  """
   def delete(self):
     if self.size == -1:
       raise IndexError("Can't pop from empty heap")
+    print('deleting:', self.storage)
     root = self.storage[0]
     # if there is more than one element in the heap
     if self.size > 0:
       self.storage[0] = self.storage[self.size]
       self._sift_down(0)
     self.size -= 1
+    print('delete:', self.storage)
     return root
 
   def get_priority(self):
@@ -66,23 +82,24 @@ class Heap:
       self._swap(parent_index, index)
       self._bubble_up(parent_index)
     return
+
   """
   Grabs the indices of this element's children and determines which child has a larger value. If the larger child's value is larger than the parent's value, the child element is swapped with the parent.
   """
   def _sift_down(self, index):
-    # If the node is a non-leaf node 
-    if not self._isLeaf(index): 
-      # and smaller than any of its child
-      if (self.storage[index] < self._leftOf(index) or self.storage[index] < self._rightOf(index)): 
-        # Swap with the left child and continue with the left child 
-        if self.comparator(self._leftOf(index), self._rightOf(index)): 
-          self._swap(index, self._left(index)) 
-          self._sift_down(self._left(index)) 
-        else: 
-          # Swap with the right child and continue with the right child 
-          self._swap(index, self._right(index)) 
-          self._sift_down(self._right(index)) 
+    current = self.storage[index]
+    leftChild_i = self._left(index)
+    leftChild_v = self._leftOf(index)
+    rightChild_i = self._right(index)
+    rightChild_v = self._rightOf(index)
 
+    bestChild_i, bestChild_v = (leftChild_i, leftChild_v)
+    if rightChild_i is not None and self.comparator(rightChild_v, leftChild_v):
+      bestChild_i, bestChild_v = (rightChild_i, rightChild_v)
+    if bestChild_i is not None and self.comparator(bestChild_v, current):
+      self.storage[index], self.storage[bestChild_i] = bestChild_v, current
+      self._sift_down(bestChild_i)
+    return
 """
 heap = Heap()
 heap.insert(6)
